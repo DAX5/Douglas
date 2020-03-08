b425754990e171e293f6747c2fcd0b5083298b95
DAX5 <44905983+DAX5@users.noreply.github.com>
2020-03-08 12:22:27 -0300

    1.3

--- js/main.js
+++ js/main.js
@@ -1,80 +1,75 @@
 (function(){
+  'use strict';
   
   var $body = document.querySelector('body');
   
-  $body.classList.remove('no-js');
-  $body.classList.add('js');
-  
-  
-  var $sections = $body.querySelectorAll('section');
-  var $icon = $body.querySelectorAll('i');
-  var $container = $body.querySelector('.container');
-  var $topMenu = $body.querySelector('nav');
-  
-  for(var i=1; i<=3; i++)
-    $sections[i].classList.add('disabled');
-  
-  for(var i = 0; i < $icon.length; i++){
-    $icon[i].addEventListener('click', clickIcon);
-  }
-  
-  
-  
-  
-  
-  function adjustHome(){
-    for(var i=0; i<$sections.length; i++){
-      if($sections[i].classList[0] === 'home' && $sections[i].classList[1] === 'actived'){
-        $container.style.maxWidth = '350px';
-        $topMenu.classList.add('disabled');
-        break;
-      }
-      else {
-        $container.style.maxWidth = '600px';
-        $topMenu.classList.remove('disabled');
-        break;
-      }
-    }
-  }
-  adjustHome();
-  
-  
-  function clickIcon(){
-    disableCurrentSection();
-    showSection(this.id);
-    adjustHome();
-  }
-  
-  function disableCurrentSection(){
-    for(var i=0; i<$sections.length; i++){
-      for(var j=0; j<$sections[i].classList.length; j++){
-        if($sections[i].classList[j] === "actived"){
-          var _a = 'li.'+$sections[i].classList[j-1];
-          var $buttom = $body.querySelector(_a);
-          $buttom.classList.remove('actived-icon');
-          
-          $sections[i].classList.remove('actived');
-          $sections[i].classList.add('disabled');
-        }
-      }
-    }
-  }
-  
-  function showSection(section){
-    var _a = 'li.'+section;
-    var $buttom = $body.querySelector(_a);
-    $buttom.classList.add('actived-icon');
+  function init() {
+    $body.classList.remove('no-js');
+    $body.classList.add('js');
     
-    for(var i=0; i<$sections.length; i++){
-      for(var j=0; j<$sections[i].classList.length; j++){
-        if($sections[i].classList[j] === section){
-          $sections[i].classList.remove('disabled');
-          $sections[i].classList.add('actived');
-        }
-      }
+    hideSections();
+    addEvents();
+    adjustContainer();
+  }
+  
+  function hideSections() {
+    var $sections = $body.querySelectorAll('section');
+    
+    for(var i = 1; i <= 3; i++)
+      $sections[i].classList.add('disabled');
+  }
+  
+  function addEvents() {
+    var $icon = $body.querySelectorAll('i');
+    
+    $icon.forEach(function(item) {
+      item.addEventListener('click', clickIcon);
+    });
+  }
+  
+  function clickIcon() {
+    var $currentSection = $body.querySelector('section.actived');
+    var $newSection = $body.querySelector('section.'+this.id);
+    
+    disableCurrentSection($currentSection);
+    showSection($newSection);
+    changeIcon($currentSection.classList[0], this.id);
+    adjustContainer();
+  }
+  
+  function adjustContainer() {
+    var $home = $body.querySelector('.home.actived');
+    var $container = $body.querySelector('.container');
+    var $topMenu = $body.querySelector('nav');
+    
+    if($home) {
+      $container.style.maxWidth = '350px';
+      $topMenu.classList.add('disabled');
+    }
+    else {
+      $container.style.maxWidth = '600px';
+      $topMenu.classList.remove('disabled');
     }
   }
   
+  function disableCurrentSection(section) {
+    section.classList.remove('actived');
+    section.classList.add('disabled');
+  }
   
+  function showSection(section) {
+    section.classList.remove('disabled');
+    section.classList.add('actived');
+  }
+  
+  function changeIcon(unselectedIcon, selectedIcon) {
+    var _a = ['li.'+unselectedIcon, 'li.'+selectedIcon];
+    var $buttons = [$body.querySelector(_a[0]), $body.querySelector(_a[1])];
+    
+    $buttons[0].classList.remove('actived-icon');
+    $buttons[1].classList.add('actived-icon');
+  }
+  
+  init();
   
 })();
